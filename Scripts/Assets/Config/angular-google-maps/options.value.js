85e0c4c574be1e5c63b5b1ac1e3032bb6e2bfd0a
ChangYinShung <changyenshung@gmail.com>
2016-09-03 14:34:26 +0800

    test

---
+++ Scripts/Assets/Config/angular-google-maps/options.value.js
@@ -0,0 +1,245 @@
+﻿/**
+* @auther ChangYinShung
+* @copyright  豐碩資訊有限公司 @ 2016
+* @description  For Value Setting
+*/
+(function (app) {
+    'use strict';
+    app.value('ycGmapValue', 預設);
+
+    /**
+     * @description 取的自訂預設 GoogleMap, map , maker 
+     * @param map {object} GoogleMap's MapOptions object
+     * @param marker {object} GoogleMap's MarkerOptions object
+     */
+
+    function 預設(map, marker) {
+
+        /**
+         * @link https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+         */
+        var defaultOption = {
+            disableDefaultUI: true,
+            draggable: true,
+            disableDoubleClickZoom: true,
+            zoomControl: false,
+            scaleControl: false,
+            scrollwheel: false,
+            styles: 預設地圖樣式()
+        };
+        angular.extend(defaultOption, map);
+
+        /**
+         * @link https://developers.google.com/maps/documentation/javascript/reference#MarkerOptions
+         */
+        var defaultMarker = {
+            options: {
+                icon: Root + 'Content/marker.png'
+            }
+        }
+        angular.extend(defaultMarker.options, marker);
+
+        var result = {
+            Map: defaultOption,
+            Marker: defaultMarker
+        }
+
+        return result;
+
+    };
+
+    function 預設地圖樣式() {
+        /**
+         * @link  https://snazzymaps.com/style/71410/usc or StylesTemplates
+         */
+        var mapStyle = [
+    {
+        "featureType": "all",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.province",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.locality",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.neighborhood",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.land_parcel",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 65
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 51
+            }
+        ]
+    },
+    {
+        "featureType": "poi.school",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            },
+            {
+                "hue": "#ff0000"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 30
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 40
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "hue": "#ffff00"
+            },
+            {
+                "lightness": -25
+            },
+            {
+                "saturation": -97
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "lightness": -25
+            },
+            {
+                "saturation": -100
+            }
+        ]
+    }
+        ]
+        return mapStyle;
+    }
+})(angular.module('uiGmapgoogle-maps'));
---
+++ Scripts/Assets/Config/angular-validation/rule-zh.config.js
@@ -0,0 +1,58 @@
+﻿(function () {
+    angular
+      .module('validation')
+      .config(['$validationProvider', function ($validationProvider) {
+          var expression = {
+              required: function (value) {
+                  return !!value;
+              },
+              url: /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/,
+              email: /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/,
+              number: /^\d+$/,
+              minlength: function (value, scope, element, attrs, param) {
+                  if (!value) return true;
+                  return value.length >= param;
+              },
+              maxlength: function (value, scope, element, attrs, param) {
+                  if (!value) return true;
+                  return value.length <= param;
+              },
+              date: function (value, scope, element, attrs, param) {
+                  return Date.parse(value);
+              }
+
+          };
+
+          var defaultMsg = {
+              required: {
+                  error: '此欄位必填',
+                  success: 'It\'s Required'
+              },
+              url: {
+                  error: 'This should be Url',
+                  success: 'It\'s Url'
+              },
+              email: {
+                  error: 'Email格式不符，請再次檢查',
+                  success: 'It\'s Email'
+              },
+              number: {
+                  error: '必須輸入 數字',
+                  success: 'It\'s Number'
+              },
+              minlength: {
+                  error: '內容太少，請充實內容。',
+                  success: 'Long enough!'
+              },
+              maxlength: {
+                  error: '內容太多，請精簡內容',
+                  success: 'Short enough!'
+              },
+              date: {
+                  error: '日期格式錯誤',
+                  success: 'It\'s Date'
+              }
+          };
+          $validationProvider.setExpression(expression).setDefaultMsg(defaultMsg);
+      }]);
+}).call(this);
