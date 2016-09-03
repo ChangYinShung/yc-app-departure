4cb4a383c61806ce6a96ebfc2f3b182fba0c382c
ChangYinShung <changyenshung@gmail.com>
2016-09-03 14:32:56 +0800

    outter

---
+++ Scripts/Profile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Optimization;
+
+namespace YC.Scripts
+{
+    public class Profile
+    {
+        protected Bundle initBundle { get; set; }
+        protected Bundle createCurrentBundle()
+        {
+            StackTrace stackTrace = new StackTrace();
+            MethodBase method = stackTrace.GetFrame(1).GetMethod();
+            BundleProfileAttribute attr = (BundleProfileAttribute)method.GetCustomAttributes(typeof(BundleProfileAttribute), true)[0];
+            string value = attr.BundleName;
+            var result = new Bundle(value);
+            result.Orderer = new AsIsBundleOrderer();
+            if (BundleTable.Bundles.GetRegisteredBundles().All(x => x.Path != result.Path))
+                BundleTable.Bundles.Add(result);
+            return result;
+        }
+    }
+    public class BundleProfileAttribute : Attribute
+    {
+        public string BundleName { get; set; }
+
+        public BundleProfileAttribute(string bundleName)
+        {
+            this.BundleName = bundleName;
+        }
+    }
+}
---
+++ Scripts/_references.js
@@ -0,0 +1,31 @@
+﻿/// <autosync enabled="false" />
+/// <reference path="App/vendor/angular/angular.js" />
+/// <reference path="angular.intellisense.js" />
+/// <reference path="App/vendor/angular-ui-router/angular-ui-router.js" />
+/// <reference path="App/vendor/restangular/restangular.js" />
+/// <reference path="App/Vendor/lodash/lodash.js" />
+/// <reference path="App/app.module.js" />
+/// <reference path="App/app.filter.js" />
+/// <reference path="App/Vendor/angular-hotkeys/hotkeys.js" />
+/// <reference path="App/Services/helper.service.js" />
+/// <reference path="App/Services/notificationService.js" />
+/// <reference path="App/Services/Api/SharedApi.service.js" />
+/// <reference path="App/Services/Utility.service.js" />
+/// <reference path="App/Services/Api/Activity.model.js" />
+/// <reference path="App/Services/Api/Activity.odata.js" />
+/// <reference path="App/Services/Api/Activity.js" />
+/// <reference path="App/Services/Api/Article.js" />
+/// <reference path="App/Services/Api/Identity.js" />
+/// <reference path="App/Services/Api/News.js" />
+/// <reference path="App/Services/Api/Slider.js" />
+/// <reference path="App/Services/Api/Topic.js" />
+/// <reference path="App/Services/Api/School.js" />
+/// <reference path="App/Services/Api/Speech.js" />
+/// <reference path="App/Services/Api/SignUp.js" />
+/// <reference path="App/Library/Stringformat.js" />
+
+/// <reference path="App/Services/Api.service.js" />
+
+
+
+
