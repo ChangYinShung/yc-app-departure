da04e2bf12376cff5de138490b266ef84c1138bd
ChangYinShung <changyenshung@gmail.com>
2016-09-03 12:11:51 +0800

    testing
    tesing

---
+++ BundleProfile/AngularGoogleMapsProfile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Optimization;
+
+namespace YC.Scripts.BundleProfile
+{
+    public class AngularGoogleMapsProfile : Profile
+    {
+        [BundleProfile("~/bundles/FurtherSoftware/Plugins/angular-google-maps")]
+        public Bundle BaseJs()
+        {
+            var bundle = createCurrentBundle();
+
+            bundle.Include(string.Concat("~/FurtherSoftware/Plugins/Vendor/angular-simple-logger/js/angular-simple-logger.js"))
+                .Include(string.Concat("~/FurtherSoftware/Plugins/Vendor/angular-google-maps/js/angular-google-maps.js"))
+                .Include(string.Concat("~/FurtherSoftware/Plugins/Config/angular-google-maps/maps.config.js"))
+                .Include(string.Concat("~/FurtherSoftware/Plugins/Config/angular-google-maps/options.value.js"));
+
+            return bundle;
+        }
+    }
+}
---
+++ BundleProfile/AngularValidationProfile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Optimization;
+
+namespace YC.Scripts.BundleProfile
+{
+    public class AngularValidationProfile : Profile
+    {
+        [BundleProfile("~/bundles/FurtherSoftware/Plugins/angular-validation")]
+        public Bundle BaseJs()
+        {
+            var bundle = createCurrentBundle();
+            bundle.Include("~/FurtherSoftware/Plugins/Vendor/angular-validation/js/angular-validation.js")
+                .Include("~/FurtherSoftware/Plugins/Config/angular-validation/rule-zh.config.js");
+            return bundle;
+        }
+        [BundleProfile("~/bundles/FurtherSoftware/Plugins/angular-validation/skyform")]
+        public Bundle SkyformJs()
+        {
+            var bundle = createCurrentBundle();
+            bundle.Include("~/FurtherSoftware/Plugins/Vendor/angular-validation/js/angular-validation.js")
+                .Include("~/FurtherSoftware/Plugins/Config/angular-validation/rule-zh.config.js")
+                .Include("~/FurtherSoftware/Plugins/Config/angular-validation/theme-skyform.config.js");
+            return bundle;
+        }
+        [BundleProfile("~/bundles/FurtherSoftware/Plugins/angular-validation/skyform/css")]
+        public Bundle SkyformCss()
+        {
+            var bundle = createCurrentBundle();
+            bundle.Include("~/Content/Front/assets/plugins/sky-forms-pro/skyforms/css/sky-forms.css")
+                .IncludeDirectory("~/Content/Front/assets/plugins/sky-forms-pro/skyforms/custom/", "*.css", true);
+            return bundle;
+        }
+
+
+
+
+    }
+}
