136be45bdc0f0fe38ae1f2fe3cefa909f72b89ab
ChangYinShung <changyenshung@gmail.com>
2016-09-03 14:36:07 +0800

    test

---
+++ Scripts/Assets/App/app.filter.js
@@ -0,0 +1,25 @@
+﻿///格式化 JSON Date
+(function (app) {
+    'use strict';
+    app.filter("jsDate", function () {
+        return function (x) {
+            if (x === null) return '';
+            return new Date(parseInt(x.substr(6)));
+        };
+    })
+})(angular.module('YC.Common'));
+
+
+//Rang
+(function (app) {
+    'use strict';
+    app.filter("range", function () {
+        return function (input, min, max) {
+            min = parseInt(min);
+            max = parseInt(max);
+        };
+    })
+})(angular.module('YC.Common'));
+
+
+
---
+++ Scripts/Assets/App/app.module.js
@@ -0,0 +1,65 @@
+﻿
+///====== ui.bootstrap:  bootstrap directive
+//======= ui.router : Router
+(function () {
+    'use strict';
+    angular.module('YC.Common', ['ui.router', 'ui.router.modal', 'ui.router.tabs', 'ui.bootstrap', 'jsonFormatter', 'cfp.hotkeys', 'nemLogging']);
+})();
+
+/**
+ * @description Plugins
+ * angular-lazy-img 圖片讀取
+ * oc.lazyLoad : Dynamic loading extra service or plugins
+ */
+(function () {
+    'use strict';
+    angular.module('YC.Plugins', ['angularLazyImg', 'oc.lazyLoad']);
+})();
+
+
+
+
+
+/**
+ * @description Restangular
+ * @description angular-toastr
+ * @description angular-local-storage
+ */
+(function () {
+    'use strict';
+    angular.module('YC.Services', [ 'toastr', 'restangular', 'ODataResources', 'LocalStorageModule'])
+})();
+
+/**
+ * @description API Service
+ * @author Furthersoft
+ */
+(function () {
+    'use strict';
+    angular.module('YC.Services.Api', ['YC.Services'])
+})();
+
+
+(function () {
+    'use strict';
+    angular.module('YC.Testing', [])
+})();
+
+/**
+ * @description 尚未整理
+ */
+(function () {
+    'use strict';
+    angular.module('YC.Services.Activity', ['YC.Services'])
+})();
+
+/**
+ * @description 尚未整理
+ */
+(function () {
+    'use strict';
+    angular.module('YC.Services.Article', ['YC.Services'])
+})();
+
+
+
